b46c4b8360a2aeed77fb6bc0bf4a2eb2e088e8f8
bhagyanimash <bhagyanimash2000@gmail.com>
2024-06-04 23:18:00 +0530

    build Setting page

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/components/my_drawer_tile.dart';
+import 'package:food_delivery_app/pages/settings_page.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
@@ -32,7 +33,12 @@ class MyDrawer extends StatelessWidget {
           MyDrawerTile(
             text: "S E T T I N G S",
             icon: Icons.settings,
-            onTap: () {},
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const SettingsPage(),
+              ),
+            ),
           ),
           const Spacer(),
           MyDrawerTile(
