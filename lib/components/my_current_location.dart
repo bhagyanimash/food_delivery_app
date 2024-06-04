201162cdd9d77890a1996c93095227ef27ae9c13
bhagyanimash <bhagyanimash2000@gmail.com>
2024-06-05 01:08:12 +0530

    build Silver App bar

---
+++ lib/components/my_current_location.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyCurrentLocation extends StatelessWidget {
+  const MyCurrentLocation({super.key});
+
+  void openLocationSearchBox(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Your Location"),
+        content: const TextField(
+          decoration: InputDecoration(hintText: "Search address.."),
+        ),
+        actions: [
+          MaterialButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("Cancle"),
+          ),
+          MaterialButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("Save"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Deliver Now",
+            style: TextStyle(color: Theme.of(context).colorScheme.primary),
+          ),
+          GestureDetector(
+            onTap: () => openLocationSearchBox(context),
+            child: Row(
+              children: [
+                Text(
+                  "No:5, Kandy, Sri Lanka",
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                      fontWeight: FontWeight.bold),
+                ),
+                Icon(Icons.keyboard_arrow_down_rounded),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_current_location.dart';
+import 'package:food_delivery_app/components/my_description_box.dart';
 import 'package:food_delivery_app/components/my_drawer.dart';
+import 'package:food_delivery_app/components/my_silver_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,10 +15,32 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
+      drawer: MyDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySilverAppBar(
+            title: const Text("title"),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Divider(
+                  indent: 25,
+                  endIndent: 25,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                //current location
+                const MyCurrentLocation(),
+
+                //description box
+                const MyDescriptionBox()
+              ],
+            ),
+          ),
+        ],
+        body: Container(
+          color: Colors.blue,
+        ),
       ),
-      drawer: const MyDrawer(),
     );
   }
 }
