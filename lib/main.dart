da9fac30b01577218b062fed2d707da1fcdd020b
bhagyanimash <bhagyanimash2000@gmail.com>
2024-07-25 01:19:40 +0530

    create: register method

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:food_delivery_app/services/auth/login_or_register.dart';
+import 'package:food_delivery_app/services/auth/auth_gate.dart';
 import 'package:food_delivery_app/firebase_options.dart';
 import 'package:food_delivery_app/models/resturent.dart';
 import 'package:food_delivery_app/themes/theme_provider.dart';
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const LoginOrRegister(),
+      home: const AuthGate(),
       theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/components/my_button.dart';
 import 'package:food_delivery_app/components/my_textfield.dart';
+import 'package:food_delivery_app/services/auth/auth_services.dart';
 
 class RegisterPage extends StatefulWidget {
   final void Function()? onTap;
@@ -15,67 +16,100 @@ class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController passwordcontroller = TextEditingController();
   final TextEditingController confirmpasswordcontroller =
       TextEditingController();
+
+  // register method
+  void register() async {
+    final _authService = AuthService();
+
+    if (passwordcontroller.text == confirmpasswordcontroller.text) {
+      try {
+        await _authService.signUpWithEmailPassword(
+            emailcontroller.text, passwordcontroller.text);
+      } catch (e) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text(e.toString()),
+          ),
+        );
+      }
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => const AlertDialog(
+          title: Text("Password doesn't match"),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.lock_open_rounded,
-              size: 100,
-              color: Theme.of(context).colorScheme.inversePrimary,
-            ),
-            const SizedBox(height: 25),
-            Text(
-              "Let's create an account for you",
-              style: TextStyle(
-                  fontSize: 16,
-                  color: Theme.of(context).colorScheme.inversePrimary),
-            ),
-            const SizedBox(height: 25),
-            MyTextField(
-                controller: emailcontroller,
-                hintText: "Email",
-                obscureText: false),
-            const SizedBox(height: 10),
-            MyTextField(
-                controller: passwordcontroller,
-                hintText: "Password",
-                obscureText: true),
-            const SizedBox(height: 10),
-            MyTextField(
-                controller: confirmpasswordcontroller,
-                hintText: "Confirm password",
-                obscureText: true),
-            const SizedBox(height: 25),
-            MyButton(text: "Sign Up", onTAp: () {}),
-            const SizedBox(height: 25),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Already have an account?",
-                  style: TextStyle(
-                      color: Theme.of(context).colorScheme.inversePrimary),
-                ),
-                const SizedBox(
-                  width: 4,
-                ),
-                GestureDetector(
-                  onTap: widget.onTap,
-                  child: Text(
-                    "Login now",
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.lock_open_rounded,
+                size: 100,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+              const SizedBox(height: 25),
+              Text(
+                "Let's create an account for you",
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.inversePrimary),
+              ),
+              const SizedBox(height: 25),
+              MyTextField(
+                  controller: emailcontroller,
+                  hintText: "Email",
+                  obscureText: false),
+              const SizedBox(height: 10),
+              MyTextField(
+                  controller: passwordcontroller,
+                  hintText: "Password",
+                  obscureText: true),
+              const SizedBox(height: 10),
+              MyTextField(
+                  controller: confirmpasswordcontroller,
+                  hintText: "Confirm password",
+                  obscureText: true),
+              const SizedBox(height: 25),
+              MyButton(
+                  text: "Sign Up",
+                  onTAp: () {
+                    register();
+                  }),
+              const SizedBox(height: 25),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account?",
                     style: TextStyle(
-                        color: Theme.of(context).colorScheme.inversePrimary,
-                        fontWeight: FontWeight.bold),
+                        color: Theme.of(context).colorScheme.inversePrimary),
                   ),
-                ),
-              ],
-            )
-          ],
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  GestureDetector(
+                    onTap: widget.onTap,
+                    child: Text(
+                      "Login now",
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.inversePrimary,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
